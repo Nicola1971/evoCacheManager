72e5bbe9158432cf66baeefc8110acf16c512e31
Nicola <nlambathakis@gmail.com>
2024-11-02 11:50:35 +0100

    Create italian.inc.php

---
+++ assets/plugins/evocachemanager/lang/italian.inc.php
@@ -0,0 +1,8 @@
+<?php
+$ecm_lang['title'] = 'Gestore della Cache';
+$ecm_lang['create'] = 'Crea Cache';
+$ecm_lang['clear'] = 'Cancella Cache';
+$ecm_lang['stop'] = ' Stop';
+$ecm_lang['refresh_cache'] = 'Nuovi file di cache saranno creati quando le pagine verranno richieste..';
+$ecm_lang['caching'] = 'Generazione della cache del sito';
+$ecm_lang['refresh'] = 'Aggiorna';
